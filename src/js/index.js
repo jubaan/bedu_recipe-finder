1c23baac3b1e68a9c30af1e4599708893fb6aa3f
francisco-solis99 <franciscosoliscss@gmail.com>
2022-08-19 19:35:18 -0500

    [Add] a image just to have an example of how we can use images in the project, this uses the html-loader but also it can be added with JS

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,7 @@
 import '../css/global.css'; //import the global css file
+import logo from '../assets/images/logoRecipeEasy.svg'; //import the logo image
+
+const header = document.querySelector('header');
+const imgElement = document.createElement('img');
+imgElement.src = logo;
+header.appendChild(imgElement);

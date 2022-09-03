f72a143419f69e50ef7ede182b06d4eb1b9358d1
Julio Añoveros <julio.ab@outlook.com>
2022-09-03 09:59:59 -0500

    feat(modal): add modal to random

--- src/js/index.js
+++ src/js/index.js
@@ -3,16 +3,51 @@ import logo from '../assets/images/logoRecipeEasy.svg'; //import the logo image
 import searchImg from '../assets/images/search-icons.svg'; //import the search image
 import randomImg from '../assets/images/random-icon.svg';
 
-const imageLogo = document.getElementById('logo')
+const imageLogo = document.getElementById('logo');
 imageLogo.src = logo;
 
 const footerLogo = document.getElementById('footer-logo');
 footerLogo.src = logo;
 
-const searchIcon = document.getElementById('searchIcon')
+const searchIcon = document.getElementById('searchIcon');
 searchIcon.src = searchImg;
 
-const randomIcon = document.getElementById('randomIcon')
+const randomIcon = document.getElementById('randomIcon');
 randomIcon.src = randomImg;
 
+const API = {
+  base: 'https://www.themealdb.com/api/json/v1/1',
+  search: '/search.php',
+  random: '/random.php',
+};
 
+function getDataInJson(url) {
+  return fetch(url)
+    .then((response) => response.json())
+    .catch((err) => console.log(err));
+}
+
+const randomButton = document.querySelector('.btn-random');
+const mealModalTemplate = document
+  .querySelector('.meal')
+  .content.querySelector('.modal')
+  .cloneNode(true);
+let body = document.body
+
+const getRandomMeal = async () => {
+  const randomUrl = `${API.base}${API.random}`;
+  const randomMeal = (await getDataInJson(randomUrl)).meals[0];
+  
+  body.append(mealModalTemplate)
+  console.log(randomMeal);
+};
+
+
+
+
+randomButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  let randomMeal = getRandomMeal;
+
+  getRandomMeal();
+});
---
+++ src/js/randomMeal.js
@@ -0,0 +1,16 @@
+let randomButton = document.querySelector('.btn-search');
+let mealModal = document
+  .querySelector('.meal').content
+  .content
+
+randomButton.addEventListener('click', (e) => {
+  e.preventDefautl();
+  console.log(mealModal);
+});
+
+let getRandomMeal = () => {
+  return new promiseS
+}
+
+
+export { randomButton };

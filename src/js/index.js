85837c49564d2a43bbd001af30b1a369335d3706
Julio Añoveros <julio.ab@outlook.com>
2022-09-04 02:37:10 -0500

    feat(modal): add ingredients

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,7 @@ import searchImg from '../assets/images/search-icons.svg'; //import the search i
 import randomImg from '../assets/images/random-icon.svg';
 import { search } from './search.js';
 import { renderCategories } from './category';
+import { API, getDataInJson } from './utils';
 
 const imageLogo = document.getElementById('logo');
 imageLogo.src = logo;
@@ -20,6 +21,7 @@ randomIcon.src = randomImg;
 // DOM elements
 const inputMeals = document.querySelector('#input-meals');
 const buttonToSearch = document.querySelector('.btn-search');
+const randomButton = document.querySelector('.btn-random');
 
 buttonToSearch.addEventListener('click', () => {
   const textToSearch = inputMeals.value;
@@ -28,3 +30,83 @@ buttonToSearch.addEventListener('click', () => {
 
 // Get gategories
 renderCategories();
+
+const mealModalTemplate = document
+  .querySelector('.meal')
+  .content.querySelector('.modal')
+  .cloneNode(true);
+const body = document.body;
+
+const getRandomMeal = async () => {
+  const randomUrl = `${API.base}${API.random}`;
+  const randomMeal = (await getDataInJson(randomUrl)).meals[0];
+
+  generateModal(randomMeal);
+
+  return randomMeal;
+};
+
+const generateModal = async (meal) => {
+  mealModalTemplate.querySelector(
+    '.meal__image'
+  ).style.backgroundImage = `url('${meal.strMealThumb}`;
+  mealModalTemplate.querySelector('.meal__name').textContent = meal.strMeal;
+  mealModalTemplate.querySelector(
+    '.meal__preparation-instructions'
+  ).textContent = meal.strInstructions;
+
+  const ingredientContainer =
+    mealModalTemplate.querySelector('.ingredients__list');
+  getIngredients(meal).forEach((ingredient) => {
+    if (ingredient && ingredient !== ' ' && ingredient !== '' && !!ingredient && (ingredient !== 'null' || ingredient !== 'null null')) {
+      let ingredientLi = document.createElement('li');
+      ingredientLi.textContent = ingredient;
+      ingredientContainer.appendChild(ingredientLi);
+    }
+  });
+
+  body.append(mealModalTemplate);
+};
+
+const getIngredients = (meal) => {
+  const ingredients = [
+    `${meal.strMeasure1} ${meal.strIngredient1}`,
+    `${meal.strMeasure2} ${meal.strIngredient2}`,
+    `${meal.strMeasure3} ${meal.strIngredient3}`,
+    `${meal.strMeasure4} ${meal.strIngredient4}`,
+    `${meal.strMeasure5} ${meal.strIngredient5}`,
+    `${meal.strMeasure6} ${meal.strIngredient6}`,
+    `${meal.strMeasure7} ${meal.strIngredient7}`,
+    `${meal.strMeasure8} ${meal.strIngredient8}`,
+    `${meal.strMeasure9} ${meal.strIngredient9}`,
+    `${meal.strMeasure10} ${meal.strIngredient10}`,
+    `${meal.strMeasure11} ${meal.strIngredient11}`,
+    `${meal.strMeasure12} ${meal.strIngredient12}`,
+    `${meal.strMeasure13} ${meal.strIngredient13}`,
+    `${meal.strMeasure14} ${meal.strIngredient14}`,
+    `${meal.strMeasure15} ${meal.strIngredient15}`,
+    `${meal.strMeasure16} ${meal.strIngredient16}`,
+    `${meal.strMeasure17} ${meal.strIngredient17}`,
+    `${meal.strMeasure18} ${meal.strIngredient18}`,
+    `${meal.strMeasure19} ${meal.strIngredient19}`,
+    `${meal.strMeasure20} ${meal.strIngredient20}`,
+  ];
+  return ingredients;
+};
+
+randomButton.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  getRandomMeal();
+});
+
+document.addEventListener('keydown', (e) => {
+  console.log(e.key);
+
+  if (e.key == 'escape') {
+    console.log(document.querySelector('.modal'));
+
+    // document.querySelector('.modal').remove;
+  }
+});
+// =======
--- src/js/utils.js
+++ src/js/utils.js
@@ -2,23 +2,81 @@ export const API = {
   base: 'https://www.themealdb.com/api/json/v1/1',
   search: '/search.php',
   random: '/random.php',
-  categories: '/categories.php', 
-  filter: '/filter.php'
-}
+  categories: '/categories.php',
+  filter: '/filter.php',
+};
 
 export function getDataInJson(url) {
   return fetch(url)
-    .then(response => response.json())
-    .catch(err => console.log(err));
+    .then((response) => response.json())
+    .catch((err) => console.log(err));
 }
 
 export function createMealCard(meal) {
-  const templateCard = document.querySelector('.meal').content.querySelector('.meals-card').cloneNode(true);
+  const mealCard = document
+    .querySelector('.meal')
+    .content.querySelector('.meals-card')
+    .cloneNode(true);
   const { strMeal, strMealThumb } = meal;
 
-  templateCard.querySelector('img').src = strMealThumb;
-  templateCard.querySelector('img').alt = strMeal;
-  templateCard.querySelector('p').textContent = strMeal;
-  console.log(templateCard);
-  return templateCard;
+  mealCard.querySelector('img').src = strMealThumb;
+  mealCard.querySelector('img').alt = strMeal;
+  mealCard.querySelector('p').textContent = strMeal;
+
+  return mealCard;
 }
+
+
+{/* <div class="modal">
+  <div class="meal">
+    <div
+      class="meal__image"
+      style="background-image: url(https://www.themealdb.com/images/media/meals/1550441882.jpg);"
+    >
+      <div class="meal__closing-button"></div>
+    </div>
+    <div class="meal__details">
+      <div class="meal__title">
+        <h2 class="meal__name"></h2>
+      </div>
+      <div class="meal__ingredients">
+        <h2 class="ingredients__title">Ingredients</h2>
+        <ul class="ingredients__list">
+          <li class="ingredient">Potatoes</li>
+          <li class="ingredient">Olive Oil</li>
+          <li class="ingredient">Bacon</li>
+          <li class="ingredient">Garlic Clove</li>
+          <li class="ingredient">Maple Syrup</li>
+          <li class="ingredient">Parsley</li>
+          <li class="ingredient">Salt</li>
+          <li class="ingredient">Pepper</li>
+          <li class="ingredient">Allspice</li>
+        </ul>
+      </div>
+
+      <div class="meal__instructions">
+        <h2 class="instructions__title">Instructions</h2>
+        <p class="meal__preparation-instructions"></p>
+      </div>
+    </div>
+  </div>
+</div>;
+
+export function createModalCard(meal) {
+  const modalCard = document
+    .querySelector('.meal')
+    .content.querySelector('.modal')
+    .cloneNode(true);
+  
+
+  modalCard.querySelector('.meal__name').textContent = meal.strMeal;
+  modalCard.querySelector('.meal__preparation-instructions').textContent = meal.strInstructions;
+  modalCard.querySelector('img').alt = strMeal;
+  modalCard.querySelector('p').textContent = strMeal;
+  console.log(modalCard);
+  return modalCard;
+}
+
+export function getAllIngredients(meal) {
+  meal.map
+} */}
